f41543d0337ec2ce98d51f4e044a2aed1874f89f
Allen Barker <Allen.L.Barker@gmail.com>
2019-05-31 15:14:10 -0400

    updated docs, changed theme

--- doc/sphinx_docs/conf.py
+++ doc/sphinx_docs/conf.py
@@ -102,7 +102,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
+#html_theme = "alabaster"
+html_theme = "sphinxdoc"
+#html_theme = "nature"
+#html_theme = "bizstyle"
+#html_theme = "traditional"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
